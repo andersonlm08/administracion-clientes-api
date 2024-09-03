80c5ea1d218c956d6056c4925258919aab5dc63d
Anderson Lopez <anderson08lm@gmail.com>
2024-09-03 18:59:43 -0500

    inicio del proyecto

---
+++ src/main/java/com/example/administracionclientesapi/AdministracionClientesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.administracionclientesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministracionClientesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministracionClientesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/administracionclientesapi/AdministracionClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.administracionclientesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministracionClientesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
